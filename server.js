a2860d8726e2c1d2756e30dc98f52d58c5784805
ShonnyAIO <exodiakah@gmail.com>
2021-09-24 00:22:17 -0400

    Finis project :D

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -8,7 +10,17 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+  let results = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+  res.json(results);
 });
 
 
